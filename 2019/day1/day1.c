c31ee0d416f8a310ec42ca635d2ad8a247f017da
Fredrik Broman <fredrik.broman@me.com>
2019-12-13 17:25:34 +0100

    [2019] clang-format all the things!

--- 2019/day1/day1.c
+++ 2019/day1/day1.c
@@ -2,28 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculate_fuel(int mass) {
-	return mass / 3 - 2;
-}
+int calculate_fuel(int mass) { return mass / 3 - 2; }
 
 int main() {
-	FILE *fp;
+  FILE *fp;
 
-	if ((fp = fopen("input.txt", "r")) == NULL ) {
-		printf("File was not opened: errno %d\n", errno);
-		exit(EXIT_FAILURE);
-	}
- 
-	char line [48];
- 	int total_fuel = 0;
+  if ((fp = fopen("input.txt", "r")) == NULL) {
+    printf("File was not opened: errno %d\n", errno);
+    exit(EXIT_FAILURE);
+  }
 
- 	while ( fgets ( line, sizeof line, fp ) != NULL )       {
- 		total_fuel += calculate_fuel(atoi(line));
-     }
-	
-	printf("%d", total_fuel);
+  char line[48];
+  int total_fuel = 0;
 
-	fclose(fp);
-	
-	exit(EXIT_SUCCESS);
+  while (fgets(line, sizeof line, fp) != NULL) {
+    total_fuel += calculate_fuel(atoi(line));
+  }
+
+  printf("%d", total_fuel);
+
+  fclose(fp);
+
+  exit(EXIT_SUCCESS);
 }
--- 2019/day4/day4.c
+++ 2019/day4/day4.c
@@ -8,47 +8,46 @@
 // give the number of elements (size)
 // as an arg
 bool contains_repeated(int n[], int s) {
-  for(int i = 0; i < s-1; i++) {
-    if(n[i+1] == n[i])
+  for (int i = 0; i < s - 1; i++) {
+    if (n[i + 1] == n[i])
       return true;
   }
   return false;
 }
 
 bool only_increases(int n[], int s) {
-  for(int i = 0; i < s-1; i++) {
-    if(!(n[i+1] >= n[i]))
+  for (int i = 0; i < s - 1; i++) {
+    if (!(n[i + 1] >= n[i]))
       return false;
   }
-      return true;
+  return true;
 }
 
 int main() {
-    int total_passwords = 0;
+  int total_passwords = 0;
 
-    // Puzzle input: 108457-562041
-    for (int num = 108457; num < 562041; num++) {
-        // convert int 123 to array[1,2,3]
-        int tmp = num;
-        int n[6]; // passwords are 6 ints long
-        for (int i = 5; i >=0; i--) {
-            n[i] = tmp % 10;
-            tmp /= 10;
-        }
-    
-        // length of array n[]
-        // ... which we already know (6) but
-        // if you ever have to figure it
-        // out during run time this is how
-        // to do it
-        int size = sizeof(n) / sizeof(n[0]); 
-
-        if (contains_repeated(n, size) && only_increases(n, size)) {
-            total_passwords++;
-        }
+  // Puzzle input: 108457-562041
+  for (int num = 108457; num < 562041; num++) {
+    // convert int 123 to array[1,2,3]
+    int tmp = num;
+    int n[6]; // passwords are 6 ints long
+    for (int i = 5; i >= 0; i--) {
+      n[i] = tmp % 10;
+      tmp /= 10;
     }
 
-      printf("total passwords: %d\n", total_passwords);
-      exit(EXIT_SUCCESS);
-}
+    // length of array n[]
+    // ... which we already know (6) but
+    // if you ever have to figure it
+    // out during run time this is how
+    // to do it
+    int size = sizeof(n) / sizeof(n[0]);
 
+    if (contains_repeated(n, size) && only_increases(n, size)) {
+      total_passwords++;
+    }
+  }
+
+  printf("total passwords: %d\n", total_passwords);
+  exit(EXIT_SUCCESS);
+}
